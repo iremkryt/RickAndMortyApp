4ac2b4d3846c098eaec30e6b3155332934e961c1
iremkryt <103690738+iremkryt@users.noreply.github.com>
2023-05-05 11:54:10 +0300

    Created characterdto

--- app/src/main/java/com/iremkryt/rickandmortyapp/data/source/remote/RickAndMortyApi.kt
+++ app/src/main/java/com/iremkryt/rickandmortyapp/data/source/remote/RickAndMortyApi.kt
@@ -1,7 +1,9 @@
 package com.iremkryt.rickandmortyapp.data.source.remote
 
+import com.iremkryt.rickandmortyapp.data.source.remote.dto.CharacterDto
 import com.iremkryt.rickandmortyapp.data.source.remote.dto.CharactersDto
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface RickAndMortyApi {
@@ -9,4 +11,9 @@ interface RickAndMortyApi {
     suspend fun getCharacters(
         @Query("page") page: Int
     ): CharactersDto
+
+    @GET("character/{id}")
+    suspend fun getCharacter(
+        @Path("id") id: Int
+    ): CharacterDto
 }
--- app/src/main/java/com/iremkryt/rickandmortyapp/data/source/remote/dto/CharactersDto.kt
+++ app/src/main/java/com/iremkryt/rickandmortyapp/data/source/remote/dto/CharactersDto.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 
 data class CharactersDto(
     @SerializedName("info")
-    val info: İnfo?,
+    val info: Info?,
     @SerializedName("results")
     val results: List<Result?>?
 )

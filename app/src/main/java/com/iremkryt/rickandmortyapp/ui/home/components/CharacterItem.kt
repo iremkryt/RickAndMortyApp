385ee091170a99a647def5f6d2579ca26dba267d
iremkryt <103690738+iremkryt@users.noreply.github.com>
2023-05-09 10:54:18 +0300

    Fixed bugs

--- app/src/main/java/com/iremkryt/rickandmortyapp/ui/home/components/CharacterItem.kt
+++ app/src/main/java/com/iremkryt/rickandmortyapp/ui/home/components/CharacterItem.kt
@@ -1,7 +1,20 @@
 package com.iremkryt.rickandmortyapp.ui.home.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import coil.size.Size
 import com.iremkryt.rickandmortyapp.domain.model.Characters
 
 @Composable
@@ -10,5 +23,65 @@ fun CharacterItem(
     item: Characters,
     onItemClicked: (Int) -> Unit
 ) {
+    Row(
+        modifier = modifier
+            .clickable { item.id?.let { onItemClicked(it) } }
+            .padding(start = 6.dp, top = 12.dp, bottom = 12.dp)
+    ) {
+        CharacterImageContainer(modifier = Modifier.size(64.dp)) {
+            CharacterImage(item)
+        }
+        Spacer(Modifier.width(20.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.CenterVertically)
+        ) {
+            item.name?.let {
+                Text(
+                    text = it,
+                    style = MaterialTheme.typography.h6
+                )
+            }
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                item.specie?.let {
+                    Text(
+                        text = it,
+                        style = MaterialTheme.typography.caption
+                    )
+                }
+            }
+        }
+        Divider(modifier = Modifier.padding(top = 10.dp))
+    }
+}
 
+@Composable
+fun CharacterImage(
+    item: Characters
+) {
+    Box {
+        val painter = rememberAsyncImagePainter(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(item.image)
+                .size(Size.ORIGINAL)
+                .build()
+        )
+        Image(
+            painter = painter,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
+
+@Composable
+fun CharacterImageContainer(
+    modifier: Modifier,
+    content: @Composable () -> Unit
+) {
+    Surface(modifier.aspectRatio(1f), RoundedCornerShape(4.dp)) {
+        content()
+    }
 }
